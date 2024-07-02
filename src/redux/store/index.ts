6c22a893386a9640a2a387e794bffec9da491841
valeria.ramirez@therocketcode.com <valeria.ramirez@therocketcode.com>
2024-07-02 02:03:32 -0600

    Config state managment with redux

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,1 +1,17 @@
-export { default as store } from './store';
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import userSliceReducer from '../users/UserReducer';
+
+const store= configureStore({
+	reducer: {
+		user: userSliceReducer,
+	},
+});
+
+export default store;
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
---
+++ src/redux/users/UserReducer.ts
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'; 
+import { User } from '@/types/users/UserTypes'; 
+import { RootState } from '@/redux/store';
+import { fetchUsers } from '@/services/users/UserListServices';
+
+ export interface UserList {
+    users: User[];
+    loading: boolean;
+    error: string | null;
+  }
+
+  const initialState: UserList = {
+    users: [],
+    loading: false,
+    error: null,
+  };
+
+  // Fetch users from API 
+export const fetchUsersAsync = createAsyncThunk(
+    'users/fetchUsers',
+    async () => {
+       const response = await fetchUsers(); 
+          
+       return response;
+    }
+  );
+
+  // Slice
+export const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+    // fetchUsers reducers
+    builder.addCase(fetchUsersAsync.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      });
+      builder.addCase(fetchUsersAsync.fulfilled, (state, action: PayloadAction<User[]>) => {
+        state.loading = false;
+        state.users = action.payload;
+      });
+      builder.addCase(fetchUsersAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || 'Error fetching users';
+      });
+    },
+});
+
+// Excport actions and reducers
+export const selectUsers = (state: RootState) => state.user.users;
+export const selectLoading = (state: RootState) => state.user.loading;
+export const selectError = (state: RootState) => state.user.error;
+
+export default userSlice.reducer;
