49edb6f61f32cf1f7c271f93edff7cab5d310fed
valeria.ramirez@therocketcode.com <valeria.ramirez@therocketcode.com>
2024-07-02 17:37:20 -0600

    Styles

--- src/components/UserListTable/UserTable.tsx
+++ src/components/UserListTable/UserTable.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '@/redux/store';
 import { useFetchUsers } from './utils';
-import UserTableRow from './UserTableRow';
+import UserTableRow from '../UserTableRow';
 import './styles.scss';
 
 
@@ -17,18 +17,18 @@ const UserTable: React.FC = () => {
     
 
   return (
-    <table className="min-w-full rounded-lg block md:table">
-      <thead className="block md:table-header-group">
+    <table className="rounded-sm text-left  border-separate border-tools-table-outline border-transparent w-full">
+      <thead className="">
         <tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto md:relative">
-          <th className="bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Id</th>
+          <th className="rounded-tl-sm bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Id</th>
           <th className="bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Nombre</th>
           <th className="bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Email</th>
           <th className="bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Genero</th>
           <th className="bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Estatus</th>
-          <th className="bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Acciones</th>
+          <th className="rounded-tl-sm bg-gray-200 p-2 text-gray-600 font-bold md:border md:border-grey-500 text-left block md:table-cell">Acciones</th>
         </tr>
       </thead>
-      <tbody className="block md:table-row-group">
+      <tbody className="block md:table-row-group rounded-b-sm">
         {users.map(user => (
           <UserTableRow key={user.id} user={user} />
         ))}
--- src/pages/UserList/UserListView.tsx
+++ src/pages/UserList/UserListView.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import CreateUserButton from '../../components/CreateUserBtn';
 import UserTable from '../../components/UserListTable';
-
+import HeaderComponent from '@/components/Header';
 
 const UserListView: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Lista de Usuarios</h1>
-      <CreateUserButton />
-      <UserTable />
+    <div className="h-screen bg-[#ebedf4]">
+      <HeaderComponent title={"Lista de usuarios"} showHomeButton={false} />
+      <div className='w-full p-8'>
+        <CreateUserButton />
+        <UserTable />
+      </div>
+
     </div>
   );
 };
