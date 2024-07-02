5fbb0671a346e36cd6d04d6ed83bd1b5e270967c
valeria.ramirez@therocketcode.com <valeria.ramirez@therocketcode.com>
2024-07-02 02:01:47 -0600

    Dependencies and configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-text': '#1b2127',
+      },
+    },
   },
   plugins: [],
 }
